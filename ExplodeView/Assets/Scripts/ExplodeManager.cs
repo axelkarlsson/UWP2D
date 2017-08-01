db4e7fb1aaf2efd86f6fd40fb4dd3a3f22d2b493
Axel <axeka054@student.liu.se>
2017-08-01 09:24:39 +0200

    Update position
    It can now update it's position yesyes, but has some unintended issues that follow from that in regards to positioning of the default unexploded state etc...

---
+++ ExplodeView/Assets/Scripts/ExplodeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeManager : MonoBehaviour
+{
+    private bool needUpdate = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdatePosition()
+    {
+        transform.position = Camera.main.transform.position;
+        needUpdate = true;
+    }
+
+    /*
+     AppCallbacks.Instance.InvokeOnAppThread(new AppCallbackItem(() =>
+                {
+                    testFunc(args);
+                }
+                ), false);
+                */
+}
