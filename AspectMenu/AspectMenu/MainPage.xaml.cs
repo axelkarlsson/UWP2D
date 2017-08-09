8e5af3ffded891baecdf64982823398f92b2ee0b
Axel <axeka054@student.liu.se>
2017-08-09 14:26:28 +0200

    Removed unnecessary button generation

--- AspectMenu/AspectMenu/MainPage.xaml.cs
+++ AspectMenu/AspectMenu/MainPage.xaml.cs
@@ -212,8 +212,8 @@ namespace AspectMenu
             
             int RowCounter = 0;
             int ColumnCounter = 0;
-            int Rmax = (int)Math.Ceiling(Math.Sqrt(GenerateButtons(uriList).Count));
-            int Cmax = (int)Math.Round(Math.Sqrt(GenerateButtons(uriList).Count));
+            int Rmax = (int)Math.Ceiling(Math.Sqrt(uriList.Count));
+            int Cmax = (int)Math.Round(Math.Sqrt(uriList.Count));
             foreach (Button b in GenerateButtons(uriList))
             {
                 BackGrid.Children.Add(b);
